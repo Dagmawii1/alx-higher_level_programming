3b328a396d367c804f6ef852388437d3b409224e
Dagmawii1 <Dagmawii1@github.com>
2022-07-06 13:36:48 -0700

    new

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    num = len(sys.argv)
-    if num == 1:
-        print("{} arguments.".format(num - 1))
-    elif num == 2:
-        print("{} argument:".format(num - 1))
-    else:
-        print("{} arguments:".format(num - 1))
 
-    for i in range(1, num):
-        print("{}: {}".format(i, sys.argv[i]))
+if __name__ == "__main__":
+    """Let's manage the arguments"""
+    import sys
+
+    index = len(sys.argv) - 1
+    if (index == 0):
+        print("0 arguments.")
+    elif (index == 1):
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(index))
+    for i in range(index):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
